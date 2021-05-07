e97b5c8235f692bf522b644fe2e6996e4b39c576
ssosohan <82721593+ssosohan@users.noreply.github.com>
2021-05-07 09:38:17 +0900

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import pandas as pd
-
-#data = pd.read_csv('C:\\Users\\소현\\Desktop\\data\\BIO_S13_S15\\S13_DAY1_EXPT 2_DRIVE.txt', sep = "\t", engine='python', encoding = "utf-8")
-#data.head()
-
-#df = pd.read_table('C:\Users\소현\Desktop\data\BIO_S13_S15\S13_DAY1_EXPT 2_DRIVE.txt', sep="\t")
-df = pd.read_table('C:\\Users\\소현\\Desktop\\data\\BIO_S13_S15\\S13_DAY1_EXPT 2_DRIVE.txt', sep="\t")
